7a0d2573f8c4f5821672aeaa97466a34dbb3851a
tomschrimshaw <tschrimshaw@gmail.com>
2025-08-09 18:55:22 +0100

    blist-wth-theme: add initial theme context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,22 @@
 import { Stack } from "expo-router/stack";
 import { StateProvider } from "../components/data/StateProvider";
+import { ThemeProvider } from "./theme/ThemeProvider";
 
 export default function Layout(): React.JSX.Element {
   return (
     <StateProvider>
-      <Stack screenOptions={
-        {
-          contentStyle: {
-            backgroundColor: '#33d2a4',
-          },
-          headerStyle: {
-            backgroundColor: '#fffefe',
-          },
-        }
-      } />
+      <ThemeProvider>
+        <Stack screenOptions={
+          {
+            contentStyle: {
+              backgroundColor: '#33d2a4',
+            },
+            headerStyle: {
+              backgroundColor: '#fffefe',
+            },
+          }
+        } />
+      </ThemeProvider>
     </StateProvider>
   );
 }
---
+++ app/theme/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+import React, { createContext } from 'react';
+import { useColorScheme } from 'react-native';
+
+export const ThemeContext = createContext({
+  theme: useColorScheme(),
+//   toggleTheme: () => {}
+});
+
+export function ThemeProvider({ children }: React.PropsWithChildren): React.JSX.Element {
+  const theme = useColorScheme();
+
+// TODO: set provider value based on native theme i.e. theme === 'dark' ? 'dark' : 'light' and set up module with dark and light colours
+// TODO: setup toggle function to switch themes
+  return (
+    <ThemeContext.Provider value={{ theme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
